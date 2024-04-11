8ad1b3fa277183e98ac916aa8c142b03fea22017
foxcaulfield <fox.caulfield@gmail.com>
2024-04-12 00:20:47 +0200

    Update weather file

--- weather.js
+++ weather.js
@@ -1,9 +1,12 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --no-warnings=ExperimentalWarning
+
 import { getArgs } from "./helpers/args.js";
 import { printHelp, printInfo, printSuccess, printError } from "./services/log.service.js";
 import fs from "node:fs";
 import path from "node:path";
-import { setKeyValue, STORAGE_CONSTANTS } from "./services/storage.service.js";
+import { getKeyValue, setKeyValue, STORAGE_CONSTANTS } from "./services/storage.service.js";
+import { WeatherAPI } from "./services/weather.service.js";
+import config from "./app.config.json"  with { type: "json" };
 
 const saveToken = async (token) => {
     try {
@@ -16,17 +19,23 @@ const saveToken = async (token) => {
 
 const initCLI = async () => {
     const args = getArgs(process.argv);
+    const weatherApi = new WeatherAPI({
+        baseUrl: config.openweater.apiUrl,
+        token: await getKeyValue(STORAGE_CONSTANTS.TOKEN)
+    });
+
 
     if (args.h) {
         // Help
         const helpFilePath = path.resolve(".", "assets", "help.txt");
         const helpText = fs.readFileSync(helpFilePath, { encoding: "utf-8" });
         printHelp(helpText);
-    } else if (args.s) {
+    } else if (args.c) {
+        console.log(await weatherApi.getCityWeather(args.c));
         // Specify
-    } else if (args.t) {
+    } else if (args.k) {
         // Token
-        await saveToken(args.t);
+        await saveToken(args.k);
     }
 
     // Weather
