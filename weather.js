94d3b4def6a10004cce18331eb579390e3772bdb
foxcaulfield <fox.caulfield@gmail.com>
2024-04-11 23:35:35 +0200

    Update weather.js file

--- weather.js
+++ weather.js
@@ -3,11 +3,11 @@ import { getArgs } from "./helpers/args.js";
 import { printHelp, printInfo, printSuccess, printError } from "./services/log.service.js";
 import fs from "node:fs";
 import path from "node:path";
-import { setKeyValue } from "./services/storage.service.js";
+import { setKeyValue, STORAGE_CONSTANTS } from "./services/storage.service.js";
 
 const saveToken = async (token) => {
     try {
-        await setKeyValue("token", token);
+        await setKeyValue(STORAGE_CONSTANTS.TOKEN, token);
         printSuccess("Token saved!");
     } catch (error) {
         printError("Token not saved! " + error.message);
