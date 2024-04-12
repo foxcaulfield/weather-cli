d8b9c5714a4fa3d900d9a2782e0d253bfdba86d4
foxcaulfield <fox.caulfield@gmail.com>
2024-04-12 14:27:33 +0200

    Update weather.js

--- weather.js
+++ weather.js
@@ -7,38 +7,10 @@ import path from "node:path";
 import { getKeyValue, setKeyValue, STORAGE_CONSTANTS } from "./services/storage.service.js";
 import { WeatherAPI } from "./services/weather.service.js";
 import config from "./app.config.json"  with { type: "json" };
+import yargs from "yargs";
+import { hideBin } from 'yargs/helpers';
+import { ArgumentParser } from "./services/args.service.js";
+import { App } from "./services/app.service.js";
 
-const saveToken = async (token) => {
-    try {
-        await setKeyValue(STORAGE_CONSTANTS.TOKEN, token);
-        printSuccess("Token saved!");
-    } catch (error) {
-        printError("Token not saved! " + error.message);
-    }
-}
-
-const initCLI = async () => {
-    const args = getArgs(process.argv);
-    const weatherApi = new WeatherAPI({
-        baseUrl: config.openweater.apiUrl,
-        token: await getKeyValue(STORAGE_CONSTANTS.TOKEN)
-    });
-
-
-    if (args.h) {
-        // Help
-        const helpFilePath = path.resolve(".", "assets", "help.txt");
-        const helpText = fs.readFileSync(helpFilePath, { encoding: "utf-8" });
-        printHelp(helpText);
-    } else if (args.c) {
-        console.log(await weatherApi.getCityWeather(args.c));
-        // Specify
-    } else if (args.k) {
-        // Token
-        await saveToken(args.k);
-    }
-
-    // Weather
-};
-
-initCLI();
+const app = new App();
+app.init();
