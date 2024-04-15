5489e6ac4d752f64fd252f814e0f78d2d4292eaa
foxcaulfield <fox.caulfield@gmail.com>
2024-04-15 09:02:55 +0200

    Move options in arg service

--- services/args.service.js
+++ services/args.service.js
@@ -7,6 +7,22 @@ import { hideBin } from 'yargs/helpers';
 
 export class ArgumentParser {
     args;
+    options = {
+        key: {
+            alias: "k",
+            describe: "Provide an API key",
+            type: "string",
+            nargs: 1,
+            demandOption: false
+        },
+        city: {
+            alias: "c",
+            describe: "Provide a target city",
+            type: "string",
+            nargs: 1,
+            demandOption: false
+        }
+    };
     #yargsLib;
     #yargsInstance;
 
@@ -18,25 +34,12 @@ export class ArgumentParser {
     }
     #setupOptions() {
         this.#yargsInstance = this.#yargsLib(hideBin(process.argv))
-            .option("key", {
-                alias: "k",
-                describe: "Provide an API key",
-                type: "string",
-                nargs: 1,
-                demandOption: false
-            })
-            .option("city", {
-                alias: "c",
-                describe: "Provide a target city",
-                type: "string",
-                nargs: 1,
-                demandOption: false
-            })
+            .options(this.options)
             .help()
             .alias("h", "help")
-            .conflicts({
-                "k": "c"
-            })
+            // .conflicts({
+            //     "k": "c"
+            // })
             .exitProcess(false);
     }
     async init() {
