1b902bff2c2308c9f76cd7bb5c39318894af7099
foxcaulfield <fox.caulfield@gmail.com>
2024-04-15 11:54:01 +0200

    Update weather service (add launch method)

--- services/weather.service.js
+++ services/weather.service.js
@@ -2,22 +2,28 @@ import axios from "axios";
 import config from "../app.config.json"  with { type: "json" };
 
 class WeatherAPI {
-    #token;
+    #baseUrl;
+    #apikey;
     #axiosInstance;
-    constructor({ baseUrl, token }) {
+    constructor({ baseUrl }) {
+        this.#baseUrl = baseUrl;
+        console.log("Weather manager instantiated");
+    }
+    async launch({ apikey }) {
         this.#axiosInstance = axios.create({
-            baseURL: baseUrl,
+            baseURL: this.#baseUrl,
             // responseType: "json",
             // transformResponse: res => res
         });
-        this.#token = token;
+        this.#apikey = apikey;
+        console.log("Weather manager initialized");
     }
     async #getWeather(lat, lon) {
         const result = await this.#axiosInstance.get(config.openweater.endpoints.weather, {
             params: {
                 lat,
                 lon,
-                appid: this.#token,
+                appid: this.#apikey,
                 lang: config.openweater.language,
                 units: "metric"
             }
@@ -28,13 +34,12 @@ class WeatherAPI {
         const result = await this.#axiosInstance.get(config.openweater.endpoints.geocodingLocation, {
             params: {
                 q: name,
-                appid: this.#token
+                appid: this.#apikey
             }
         });
         return (result.data)[0];
     }
     async getCityWeather(name) {
-        console.log(this.#axiosInstance);
         const cityData = await this.#getCity(name);
         return await this.#getWeather(cityData.lat, cityData.lon);
     }
